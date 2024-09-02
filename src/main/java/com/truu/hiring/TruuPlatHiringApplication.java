bd0d6821bebc82af423d3bba424524ed9574afec
Roman Valina <rvalina@truu.ai>
2024-09-02 13:47:36 +0200

    Added spring application.

---
+++ src/main/java/com/truu/hiring/TruuPlatHiringApplication.java
@@ -0,0 +1,18 @@
+package com.truu.hiring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TruuPlatHiringApplication {
+/*
+
+  Logger if debug
+  some password in plain text, or certificate in plain text
+
+ */
+  public static void main(String[] args) {
+    SpringApplication.run(TruuPlatHiringApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/truu/hiring/TruuPlatHiringProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.truu.hiring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TruuPlatHiringProjectApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
