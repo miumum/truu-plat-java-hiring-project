206c16b65569ef3bed9602daca8ad4e63cc55979
Roman Valina <rvalina@truu.ai>
2024-09-02 14:18:21 +0200

    Added spring security.

--- src/main/java/com/truu/hiring/config/ApplicationConfig.java
+++ src/main/java/com/truu/hiring/config/ApplicationConfig.java
@@ -2,9 +2,18 @@ package com.truu.hiring.config;
 
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.ScheduledThreadPoolExecutor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.concurrent.CustomizableThreadFactory;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.www.BasicAuthenticationEntryPoint;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 
 /**
@@ -21,4 +30,5 @@ public class ApplicationConfig {
     executorService.setRemoveOnCancelPolicy(true);
     return executorService;
   }
+
 }
