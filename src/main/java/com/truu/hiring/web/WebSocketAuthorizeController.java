3b30b1e96f62fdee6ebcbb70d43e528e8b0d3f9e
Roman Valina <rvalina@truu.ai>
2024-09-02 14:29:38 +0200

    Removed logger

--- src/main/java/com/truu/hiring/web/WebSocketAuthorizeController.java
+++ src/main/java/com/truu/hiring/web/WebSocketAuthorizeController.java
@@ -26,8 +26,6 @@ import org.springframework.web.socket.messaging.SessionUnsubscribeEvent;
 @Controller
 public class WebSocketAuthorizeController {
 
-  public static final Logger LOGGER = LoggerFactory.getLogger(WebSocketAuthorizeController.class);
-
   public static final String RESOLVED_REQUEST_QUEUE = "/queue" + "/requestResolved";
 
   protected final ScheduledExecutorService scheduledExecutorService;
@@ -59,7 +57,7 @@ public class WebSocketAuthorizeController {
       String wsSubscriptionId = simpMessage.getSubscriptionId();
       String identityRequestId = simpMessage.getMessageHeaders().get("identityRequestId", String.class);
       Principal user = event.getUser();
-      LOGGER.debug("Adding listener {} to request resolved status for wsSessionId {}", wsSubscriptionId, wsSessionId);
+
 
       var eventSubscriptionKey = new IdentityRequestSubscriptionKey(
           wsSessionId,
@@ -70,7 +68,6 @@ public class WebSocketAuthorizeController {
       startToPublishResolutionEvents(eventSubscriptionKey);
     } catch (Exception e) {
       String msg = "Could not subscribe to " + RESOLVED_REQUEST_QUEUE;
-      LOGGER.error(msg, e);
       throw new IllegalStateException(msg, e);
     }
   }
@@ -81,8 +78,6 @@ public class WebSocketAuthorizeController {
       SimpMessageHeaderAccessor simpMessage = StompHeaderAccessor.wrap(event.getMessage());
       String wsSessionId = simpMessage.getSessionId();
       String wsSubscriptionId = simpMessage.getSubscriptionId();
-      LOGGER.debug("Removing listener {} to request resolved status for  wsSessionId {}", wsSubscriptionId,
-          wsSessionId);
 
       var toRemove = listeningIdentityRequests.keySet().stream()
           .filter(subscriptionKey ->
@@ -95,7 +90,6 @@ public class WebSocketAuthorizeController {
       }
     } catch (Exception e) {
       String msg = "Could not release the resources while unsubscribe from " + RESOLVED_REQUEST_QUEUE;
-      LOGGER.error(msg, e);
       throw new IllegalStateException(msg, e);
     }
   }
@@ -105,7 +99,6 @@ public class WebSocketAuthorizeController {
     try {
       SimpMessageHeaderAccessor simpMessage = StompHeaderAccessor.wrap(event.getMessage());
       String wsSessionId = simpMessage.getSessionId();
-      LOGGER.debug("Removing listeners for request resolved status events for wsSessionId {}", wsSessionId);
 
       var toRemove = listeningIdentityRequests.keySet().stream()
           .filter(subscriptionKey -> subscriptionKey.wsSessionId.equals(wsSessionId))
@@ -116,7 +109,6 @@ public class WebSocketAuthorizeController {
       }
     } catch (Exception e) {
       String msg = "Could not release the resources while disconnect from ws session.";
-      LOGGER.warn(msg, e);
       throw new IllegalStateException(msg, e);
     }
   }
@@ -124,8 +116,6 @@ public class WebSocketAuthorizeController {
   private void startToPublishResolutionEvents(IdentityRequestSubscriptionKey eventSubscriptionKey) {
 
     var identityRequestPublisher = prepareIdentityRequestPublisher(eventSubscriptionKey);
-    LOGGER.info("Starting listening for identity request {} status change, websocket subscription: {}",
-        eventSubscriptionKey.identityRequestId, eventSubscriptionKey);
     ScheduledFuture<?> scheduledFuture = scheduledExecutorService.scheduleWithFixedDelay(
         identityRequestPublisher,
         1000,
@@ -141,14 +131,11 @@ public class WebSocketAuthorizeController {
       String identityRequestId = eventSubscriptionKey.identityRequestId;
       String wsUserName = eventSubscriptionKey.wsUserName;
 
-      LOGGER.trace("Reading identity request {} status, subscription: {}", identityRequestId, eventSubscriptionKey);
       var identityStatus = requestManager.getRequestState(identityRequestId);
 
       String upn = identityStatus.getUpn();
       Status status = identityStatus.getStatus();
 
-      LOGGER.info("Identity request {} resolved with status {} by {}, listener removed for subscription {}",
-                  identityRequestId, status, upn, eventSubscriptionKey);
       wsMessagingTemplate.convertAndSendToUser(
           wsUserName,
           RESOLVED_REQUEST_QUEUE,
@@ -213,13 +200,10 @@ public class WebSocketAuthorizeController {
       var removed = super.get(key);
       if (removed != null) {
         if (removed.isDone()) {
-          LOGGER.debug("Removing task for {}", key);
           return super.remove(key);
         } else {
-          LOGGER.debug("Canceling task for {}", key);
           boolean canceled = removed.cancel(mayInterruptIfRunning);
           if (canceled) {
-            LOGGER.debug("Removing canceled task for {}", key);
             return super.remove(key);
           } else {
             return null;
